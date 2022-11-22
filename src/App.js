aaa065df63c07f6dcafa9ee35d914f73e38f0f16
Victor-Mannelli <imp.mannelli@gmail.com>
2022-11-22 13:17:18 -0300

    feat: possible routs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CheckoutPage from "./pages/checkout";
+import LoginPage from "./pages/login";
+import MyCartPage from "./pages/myCart";
+import PageNotFound from "./pages/pageNotFound";
+import RegistrationPage from "./pages/registration";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="" />
-        <Route path="" />
-        <Route path="" />
-        <Route path="" />
-        <Route path="" />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="*" element={<PageNotFound />} />
+				<Route path="/" element={<LoginPage />} />
+				<Route path="/registration" element={<RegistrationPage />} />
+				<Route path="/cart" element={<MyCartPage />} />
+				<Route path="/checkout" element={<CheckoutPage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
-
