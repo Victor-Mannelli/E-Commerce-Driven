4dcb0f0da7e8c1c1b2d11fd6bc55257d103d9656
Victor-Mannelli <imp.mannelli@gmail.com>
2022-11-27 18:24:40 -0300

    fix: merge

--- src/GeneralStyles.js
+++ src/GeneralStyles.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import backgroundImage from "./assets/background.png";
 import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
+import backgroundImage from "./assets/background.png";
+import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
 
 export const PageDefaultStyle = styled.div`
 	display: flex;
@@ -15,8 +17,31 @@ export const PageDefaultStyle = styled.div`
 	@media (max-width: 993px) {
 		padding-top: 0;
 	}
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding-top: 70px;
+	width: 100%;
+	min-height: 100vh;
+	background-image: url(${backgroundImage});
+	background-size: auto;
+	cursor: default;
+	@media (max-width: 993px) {
+		padding-top: 0;
+	}
 `;
 export const StyledInput = styled.input`
+	width: 340px;
+	height: 40px;
+	border-radius: 5px;
+	border: none;
+	outline: none;
+	padding: 0 37px 0 15px;
+	font-family: "Roboto", sans-serif;
+	font-size: 17px;
+	@media (max-width: 560px) {
+		width: 100%;
+	}
 	width: 340px;
 	height: 40px;
 	border-radius: 5px;
@@ -51,8 +76,25 @@ export const StyledRegistrationPage = styled.div`
 			filter: brightness(0.6);
 		}
 	}
+  .back {
+    font-size: 12px;
+  }
 `;
 export const StyledRegistrationInput = styled.input`
+	background: white;
+	border: none;
+	border-radius: 5px;
+	height: 45px;
+	width: 100%;
+	margin-bottom: 8px;
+	padding: 0 15px;
+	font-size: 18px;
+	display: flex;
+	align-items: center;
+	color: black;
+	&::placeholder {
+		color: #868686;
+	}
 	background: white;
 	border: none;
 	border-radius: 5px;
@@ -82,6 +124,49 @@ export const StyledButton = styled.button`
 	:hover {
 		background-color: gray;
 	}
+	width: 100%;
+	height: 46px;
+	margin-top: 10px;
+	background: #262a2b;
+	border-radius: 5px;
+	border: none;
+	color: white;
+	font-weight: 700;
+	font-size: 20px;
+	cursor: pointer;
+	:hover {
+		background-color: gray;
+	}
+`;
+export const LoadingPage = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding-top: 70px;
+	width: 100%;
+	min-height: 100vh;
+	background-image: url(${backgroundImage});
+	cursor: default;
+	img {
+		width: 100px;
+		height: 100px;
+	}
+`;
+export const SearchBar = styled.div`
+	position: relative;
+	padding: 20px 0;
+	margin: 0 auto;
+	width: 340px;
+	@media (max-width: 560px) {
+		width: 80%;
+	}
+`;
+export const SearchIcon = styled(HiOutlineMagnifyingGlass)`
+	position: absolute;
+	top: 28px;
+	right: 7px;
+	width: 25px;
+	height: 25px;
 `;
 export const LoadingPage = styled.div`
 	display: flex;
--- src/pages/purchaseSelection.js
+++ src/pages/purchaseSelection.js
@@ -6,132 +6,143 @@ import axios from "axios";
 import UserContext from "../contexts/UserContext";
 
 export default function PurchaseSelection() {
-	const navigate = useNavigate();
-	const { id } = useParams();
-	const [counter, setCounter] = useState(0);
-	const [productsList, setProductsList] = useState([]);
-	const [price, setPrice] = useState(0);
-	const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [counter, setCounter] = useState(0);
+  const [productsList, setProductsList] = useState([]);
+  const [price, setPrice] = useState(0);
+  const { user } = useContext(UserContext);
 
-	useEffect(() => {
-		axios
-			.get("http://localhost:5000/products")
-			.then((e) => setProductsList(e.data.allproducts))
-			.catch((e) => console.log(e));
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-	console.log(productsList)
-	function HandlePurchase() {
-		const purchase = {
-			product: currentProduct?.name,
-			quantity: counter,
-			user,
-		};
-		axios
-			.post("http://localhost:5000/cart", purchase)
-			.then((e) => console.log(e))
-			.catch((e) => console.log(e));
-	}
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/products")
+      .then((e) => setProductsList(e.data.allproducts))
+      .catch((e) => console.log(e));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-	useEffect(() => {
-		const aux = currentProduct?.price.replace("$", "").replace("/kg","");
-		setPrice((aux * counter).toFixed(2));
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [counter]);
+  function HandlePurchase() {
+    if (!user) {
+      navigate("/login");
+    } else {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    };
+    const purchase = {
+      product: currentProduct,
+      quantity: counter,
+    };
+    axios
+      .post("http://localhost:5000/cart", purchase, config)
+      .then(() => navigate("/"))
+      .catch((e) => console.log(e));
+  }
+}
 
-	let currentProduct = productsList.find((e) => e._id === id);
-	return (
-		<InsideProduct>
-			<img src={currentProduct?.image} alt="" />
-			<h1> {currentProduct?.name} </h1>
-			<div>
-				<QuantityDiv>
-					<div onClick={() => counter > 0 && setCounter(counter - 1)}>
-						<AiOutlineMinus style={{ cursor: "pointer" }} />
-					</div>
-					<p> {counter} </p>
-					<div
-						onClick={() =>
-							counter < Number(currentProduct?.stock) && setCounter(counter + 1)
-						}
-					>
-						<AiOutlinePlus style={{ cursor: "pointer" }} />
-					</div>
-				</QuantityDiv>
-				<PurchaseDiv>
-					<div style={{ cursor: "pointer" }} onClick={() => HandlePurchase}>
-						<p> Add to Cart </p>
-						<p> {counter === 0 ? currentProduct?.price : `$${price}`} </p>
-					</div>
-					<div style={{ cursor: "pointer" }} onClick={() => navigate("/")}>
-						<p> Cancel </p>
-					</div>
-				</PurchaseDiv>
-			</div>
-		</InsideProduct>
-	);
+  useEffect(() => {
+    const aux = currentProduct?.price.replace("$", "").replace("/kg", "");
+    setPrice((aux * counter).toFixed(2));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [counter]);
+
+  let currentProduct = productsList.find((e) => e._id === id);
+  return (
+    <InsideProduct>
+      <img src={currentProduct?.image} alt="" />
+      <h1> {currentProduct?.name} </h1>
+      <div>
+        <QuantityDiv>
+          <div onClick={() => counter > 0 && setCounter(counter - 1)}>
+            <AiOutlineMinus style={{ cursor: "pointer" }} />
+          </div>
+          <p> {counter} </p>
+          <div
+            onClick={() =>
+              counter < Number(currentProduct?.stock) && setCounter(counter + 1)
+            }
+          >
+            <AiOutlinePlus style={{ cursor: "pointer" }} />
+          </div>
+        </QuantityDiv>
+        <PurchaseDiv>
+          <div
+            style={{ cursor: "pointer" }}
+            onClick={() => HandlePurchase()}
+          >
+            <p> Add to Cart </p>
+            <p> {counter === 0 ? currentProduct?.price : `$${price}`} </p>
+          </div>
+          <div style={{ cursor: "pointer" }} onClick={() => navigate("/")}>
+            <p> Cancel </p>
+          </div>
+        </PurchaseDiv>
+      </div>
+    </InsideProduct>
+  );
 }
 const InsideProduct = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-	gap: 20px;
-	width: 100%;
-	min-height: 100vh;
-	background-color: var(--darkmode);
-	cursor: default;
-	img {
-		height: 250px;
-		width: 450px;
-		border-radius: 5px;
-	}
-	h1 {
-		color: var(--darkmodeText);
-		font-size: 25px;
-		font-family: "Roboto", sans-serif;
-	}
-	div {
-		font-family: "Roboto", sans-serif;
-	}
+  gap: 20px;
+  width: 100%;
+  min-height: 100vh;
+  background-color: var(--darkmode);
+  cursor: default;
+  img {
+    height: 250px;
+    width: 450px;
+    border-radius: 5px;
+  }
+  h1 {
+    color: var(--darkmodeText);
+    font-size: 25px;
+    font-family: "Roboto", sans-serif;
+  }
+  div {
+    font-family: "Roboto", sans-serif;
+  }
 `;
 const QuantityDiv = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-	width: 200px;
-	height: 40px;
-	border-radius: 5px;
-	border: none;
-	margin: 5px 0;
-	background-color: lightgray;
-	div {
-		display: flex;
-		justify-content: center;
-		align-items: center;
+  width: 200px;
+  height: 40px;
+  border-radius: 5px;
+  border: none;
+  margin: 5px 0;
+  background-color: lightgray;
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-		width: 70px;
-		height: 100%;
-		border-radius: 5px;
-		cursor: pointer;
-		:hover {
-			background-color: gray;
-		}
-	}
+    width: 70px;
+    height: 100%;
+    border-radius: 5px;
+    cursor: pointer;
+    :hover {
+      background-color: gray;
+    }
+  }
 `;
 const PurchaseDiv = styled.div`
-	div {
-		display: flex;
-		justify-content: space-around;
-		align-items: center;
+  div {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
 
-		width: 200px;
-		height: 40px;
-		border-radius: 5px;
-		border: none;
-		margin-bottom: 5px;
-		background-color: lightgray;
-	}
+    width: 200px;
+    height: 40px;
+    border-radius: 5px;
+    border: none;
+    margin-bottom: 5px;
+    background-color: lightgray;
+  }
 `;
